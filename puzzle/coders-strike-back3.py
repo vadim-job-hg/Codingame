34a76dda1c23dd7180192383273737f1d9fdb708
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-14 17:26:07 +0300

    coders

--- puzzle/coders-strike-back3.py
+++ puzzle/coders-strike-back3.py
@@ -12,17 +12,22 @@ while True:
     # next_checkpoint_y: y position of the next check point
     # next_checkpoint_dist: distance to the next checkpoint
     # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
-    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
+    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in
+                                                                                               input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr)
-    if next_checkpoint_angle>90 or next_checkpoint_angle<-90:
-        trust = 20
+    trust = 0
+    if next_checkpoint_angle > 90 or next_checkpoint_angle < -90:
+        trust = str(10)
     else:
-        trust = 100
+        if trust < 50:
+            trust = 'BOOST'
+        else:
+            trust = str(100)
 
     # You have to output the target position
     # followed by the power (0 <= thrust <= 100)
     # i.e.: "x y thrust"
-    print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(trust))
+    print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + trust)

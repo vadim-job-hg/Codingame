f160658b95ae3060a72df6cbf8d7a71dedceb533
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-14 16:45:41 +0300

    coders

---
+++ puzzle/coders-strike-back.py
@@ -0,0 +1,25 @@
+# https://www.codingame.com/ide/puzzle/coders-strike-back
+import sys
+import math
+
+# This code automatically collects game data in an infinite loop.
+# It uses the standard input to place data into the game variables such as x and y.
+# YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+
+
+# game loop
+while True:
+    # x: x position of your pod
+    # y: y position of your pod
+    # next_checkpoint_x: x position of the next check point
+    # next_checkpoint_y: y position of the next check point
+    x, y, next_checkpoint_x, next_checkpoint_y = [int(i) for i in input().split()]
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+
+    # Edit this line to output the target position
+    # and thrust (0 <= thrust <= 100)
+    # i.e.: "x y thrust"
+    print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " 50")

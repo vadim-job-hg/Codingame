739712d109b3b534b25c1a279aa4ef61cb227a18
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-14 14:00:27 +0300

    onboad

---
+++ puzzle/onboarding.py
@@ -0,0 +1,16 @@
+# https://www.codingame.com/ide/puzzle/onboarding
+
+# game loop
+while 1:
+    enemy_1 = input()  # name of enemy 1
+    dist_1 = int(input())  # distance to enemy 1
+    enemy_2 = input()  # name of enemy 2
+    dist_2 = int(input())  # distance to enemy 2
+
+    # Write an action using print
+
+    # Enter the code here
+    if dist_1 < dist_2:
+        print(enemy_1)
+    else:
+        print(enemy_2)

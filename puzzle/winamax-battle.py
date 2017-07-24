3bdd4fc125326c182b2773aaff16a530337270cd
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-24 18:04:27 +0300

    coders

--- puzzle/winamax-battle.py
+++ puzzle/winamax-battle.py
@@ -3,7 +3,7 @@ import sys
 import math
 
 
-class Game:
+class Game():
     KARTS = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13,
              'A': 14}
     _player1deck = []

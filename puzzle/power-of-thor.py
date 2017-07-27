5ec4500a77088bc1ff3e03760dc452e43ee804e3
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-27 17:04:16 +0300

    coders

--- puzzle/power-of-thor.py
+++ puzzle/power-of-thor.py
@@ -1,5 +1,6 @@
 x,y,z,w=[int(i)for i in input().split()]
 while 1:
+ #m = (5, 100)[t == 0]
  m,w=["S",w+1] if w<y else ["N",w-1] if w>y else ["",w]
  m,z=[m+"E",z+1] if z<x else [m+"W",z-1] if z>x else [m,z] 
  print m

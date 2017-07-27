99fb8a2865cdb20fb03652dc9ae933a183de98ba
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-27 17:21:30 +0300

    coders

--- puzzle/power-of-thor.py
+++ puzzle/power-of-thor.py
@@ -1,6 +1,5 @@
 x,y,z,w=[int(i)for i in input().split()]
 while 1:
- #m = (5, 100)[t == 0]
- m,w=["S",w+1] if w<y else ["N",w-1] if w>y else ["",w]
- m,z=[m+"E",z+1] if z<x else [m+"W",z-1] if z>x else [m,z] 
- print m
+ m,w=(((["",w],["N",w-1])[w>y]),["S",w+1])[w<y]
+ m,z=((([m,z],[m+"W",z-1])[z>x]),[m+"E",z+1])[z<x]
+ print(m)

52adbada7442ee6932649cdb099a6535e933a24d
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-07-29 16:35:11 +0300

    #the-last-crusade-episode

--- CODE GOLF/temperature-code-golf.py
+++ CODE GOLF/temperature-code-golf.py
@@ -1,5 +1,26 @@
-n,a=int(input()),[int(i)for i in input().split()]
-m=(0,2**9)[n>0]
-for i in a:
- m=(m,i)[abs(m)>abs(i)or(abs(m)==abs(i)and i>m)]
-print(m)
+elv,elva={},[]
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
+for i in range(nb_elevators):
+ elva.append([int(j) for j in input().split()])
+ elv[elva[i][0]]=elva[i][1]
+while 1:
+ clone_floor, clone_pos, direction = input().split()
+ clone_floor, clone_pos = int(clone_floor), int(clone_pos)
+ if clone_floor==exit_floor or clone_floor==-1 :
+  #p = ()[]
+  if clone_pos>exit_pos and direction=='RIGHT':
+   p ='BLOCK'
+  elif clone_pos<exit_pos and direction=='LEFT':
+   p = 'BLOCK'
+  else:
+   p = "WAIT"
+ else:
+  if nb_floors>0:
+   posi=elv[clone_floor]
+   if clone_pos>posi and direction=='RIGHT':
+    p = 'BLOCK'
+   elif clone_pos<posi and direction=='LEFT':
+    p = 'BLOCK'
+   else:
+    p ="WAIT"
+ print(p)

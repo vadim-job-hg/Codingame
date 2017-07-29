4c89960cbd5fac7f53875fb204b672cb7c3bc2d1
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-07-29 16:47:51 +0300

    #the-last-crusade-episode

--- CODE GOLF/temperature-code-golf.py
+++ CODE GOLF/temperature-code-golf.py
@@ -1,4 +1,4 @@
-elv,elva={},[]
+elv,elva, W,B={},[],"WAIT",'BLOCK'
 nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
 for i in range(nb_elevators):
  elva.append([int(j) for j in input().split()])
@@ -7,7 +7,7 @@ while 1:
  clone_floor, clone_pos, direction = input().split()
  clone_floor, clone_pos = int(clone_floor), int(clone_pos)
  if clone_floor==exit_floor or clone_floor==-1 :
-  p = ("WAIT", 'BLOCK')[(clone_pos>exit_pos and direction=='RIGHT')or(clone_pos<exit_pos and direction=='LEFT')]
+  p = (W, B)[(clone_pos>exit_pos and direction=='RIGHT')or(clone_pos<exit_pos and direction=='LEFT')]
  else:
   posi = elv[clone_floor]
   p = ("WAIT", 'BLOCK')[(clone_pos>posi and direction=='RIGHT') or (clone_pos<posi and direction=='LEFT')]

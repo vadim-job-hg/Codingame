80da5da5046619a917d235acf7575c8526ad6175
Винямин Карлович Штольц <info@knubiosoft.com>
2017-07-31 15:39:17 +0300

    Merge branch 'master' of https://github.com/vadim-job-hg/Codingame
    # Conflicts:
    #	Coders Strike Back/coders-strike-back5-0.py

--- CODE GOLF/temperature-code-golf.py
+++ CODE GOLF/temperature-code-golf.py
@@ -1,13 +1,5 @@
-e,v,W,B={},[],"WAIT",'BLOCK'
-o,l,u,z,s,t,a,r=[int(i) for i in input().split()]
-for i in range(r):
- v.append([int(j) for j in input().split()])
- e[v[i][0]]=v[i][1]
-while 1:
- f,p,d=input().split()
- f,p=int(f),int(p)
- if f==z or f==-1:
-  y=(W,B)[(p>s and d[:1]=='R')or(p<s and d[:1]=='L')]
- else:
-  y=(W,B)[(p>e[f] and d[:1]=='R')or(p<e[f] and d[:1]=='L')]
- print(y)
+n,a=int(input()),[int(i)for i in input().split()]
+m=(0,2**9)[n>0]
+for i in a:
+ m=(m,i)[abs(m)>abs(i)or(abs(m)==abs(i)and i>m)]
+print(m)

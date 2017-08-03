4959ccf02f63fa8a482d6ebeca279a7ea10451a0
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-03 15:45:14 +0300

    Merge branch 'master' of https://github.com/vadim-job-hg/Codingame
    # Conflicts:
    #	Coders Strike Back/coders-strike-back5-0.py

--- WONDEV WOMAN/wondev-woman-wood-1.py
+++ WONDEV WOMAN/wondev-woman-wood-1.py
@@ -139,15 +139,35 @@ class Action():
 
     def get_best_variant_to_move(self):
         best_action = None
-        best_val = -1
+        best_val = best_val2 = -1
+        # print(self.legal_actions_list, file=sys.stderr)
         for action in self.legal_actions_list:
             cur_p = self.players[action['index']]
             cur_h = int(self.area.get_value(cur_p.x, cur_p.y))
             try:
                 if (action['atype'] == 'PUSH&BUILD'):
-                    pass
-                    # if is_enemy_oncoors():
-                    # check is enemy here
+                    print(action, file=sys.stderr)
+                    push_coors = self.area.get_corrs_from_direction(cur_p.x, cur_p.y, action['dir_1'])
+                    push_height = self.area.get_value(move_coors['x'], move_coors['y'])
+                    if push_height == '.':
+                        continue
+                    push_dir = self.area.get_corrs_from_direction(push_coors['x'], push_coors['y'],
+                                                                  action['dir_2'])
+                    push_dir_height = self.area.get_value(push_coors['x'], push_coors['y'])
+
+                    if push_dir_height == '.':
+                        continue
+
+                    if ((1 <= cur_h <= 3) and 2 < int(push_height) < MAX_HEIGHT and int(push_dir_height) < 2
+                        and self.check_build_height(push_dir_height)
+                        and not (self.is_friend_on_pos(push_dir['x'], push_dir['y'], action['index']))
+                        and self.is_enemy_on_pos(push_coors['x'], push_coors['y'])
+                        and not (self.is_enemy_on_pos(push_dir['x'], push_dir['y']))
+                        ):
+                        best_val = int(push_height)
+                        best_val2 = int(push_dir_height)
+                        best_action = action
+                        break
                 else:
                     move_coors = self.area.get_corrs_from_direction(cur_p.x, cur_p.y, action['dir_1'])
                     move_height = self.area.get_value(move_coors['x'], move_coors['y'])
@@ -158,22 +178,27 @@ class Action():
                     build_height = self.area.get_value(build_coors['x'], build_coors['y'])
                     if build_height == '.':
                         continue
-                    if (self.check_move_height(best_val, move_height, cur_h) and self.check_build_height(
-                            build_height)
+                    if (self.check_move_height(best_val, best_val2, move_height, build_height,
+                                               cur_h) and self.check_build_height(
+                            build_height)  # and int(build_height)>=best_val2
                         and not (self.is_friend_on_pos(move_coors['x'], move_coors['y'], action['index']))
                         and not (self.is_friend_on_pos(build_coors['x'], build_coors['y'], action['index']))
                         and not (self.is_enemy_on_pos(move_coors['x'], move_coors['y']))
                         and not (self.is_enemy_on_pos(build_coors['x'], build_coors['y']))
                         ):
                         best_val = int(move_height)
+                        best_val2 = int(build_height)
                         best_action = action
 
             except IndexError:
                 continue
-            return best_action
+        return best_action
 
-    def check_move_height(self, best_val, move_height, cur_h):
-        return best_val <= int(move_height) and int(move_height) <= cur_h + 1 and int(move_height) < MAX_HEIGHT
+    def check_move_height(self, best_val, best_val2, move_height, build_height, cur_h):
+        return (best_val < int(move_height) or (
+        (int(build_height) <= int(move_height) + 1 or int(build_height) + 1 == int(cur_h)) and best_val == int(
+            move_height)) and best_val2 < int(build_height)) and int(move_height) <= cur_h + 1 and int(
+            move_height) < MAX_HEIGHT
 
     def check_build_height(self, build_height):
         return int(build_height) < MAX_HEIGHT

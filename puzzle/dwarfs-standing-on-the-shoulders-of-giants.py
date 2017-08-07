271a35de2e32fe9dee5204b4f8cc0bf706b7acc3
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-07 11:10:23 +0300

    Merge branch 'master' of https://github.com/vadim-job-hg/Codingame
    # Conflicts:
    #	Coders Strike Back/coders-strike-back5-0.py

---
+++ puzzle/dwarfs-standing-on-the-shoulders-of-giants.py
@@ -0,0 +1,41 @@
+# https://www.codingame.com/ide/puzzle/dwarfs-standing-on-the-shoulders-of-giants
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+connection = {}
+
+
+def letsCount(number):
+    print("number {0}".format(number), file=sys.stderr)
+    global connection
+    max = 0
+    get_array = connection.get(number, [])
+    if len(get_array) == 0:
+        return 0
+    else:
+        for item in get_array:
+            temp = letsCount(item)
+            if max < temp:
+                max = temp
+    return max + 1
+
+
+n = int(input())  # the number of relationships of influence
+for i in range(n):
+    # x: a relationship of influence between two people (x influences y)
+    x, y = [int(j) for j in input().split()]
+    print(x, y, file=sys.stderr)
+    connection.setdefault(x, []).append(y)
+print(connection, file=sys.stderr)
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+max = 0
+for item in connection:
+    temp = letsCount(item)
+    if max < temp:
+        max = temp
+
+# The number of people involved in the longest succession of influences
+print(max + 1)

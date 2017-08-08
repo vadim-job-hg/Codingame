64d581467710614a97b8f2afe739b4b01c5b0343
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-08 12:08:04 +0300

    Gift

---
+++ puzzle/the-gift.py
@@ -0,0 +1,48 @@
+# https://www.codingame.com/ide/puzzle/the-gift
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+budget_left = []
+budget_calculated=[]
+oods_left = n = int(input())
+cost_left = int(input())
+for i in range(n):
+    budget_left.append(int(input()))
+    budget_calculated.append(0)
+print("budget_left {0}".format(budget_left), file=sys.stderr)
+print("cost_left {0}".format(cost_left), file=sys.stderr)
+while oods_left>0:
+    each_pay = math.floor(cost_left/oods_left)
+    for i in range(n):
+        if budget_left[i]==0:
+            continue
+        if(budget_left[i]<=each_pay):
+            oods_left-=1
+            budget_calculated[i] += budget_left[i]
+            cost_left -= budget_left[i]
+            budget_left[i] = 0
+        else:
+            budget_calculated[i] += each_pay
+            cost_left -= each_pay
+            budget_left[i] -= each_pay
+        print("budget_calculated {0}".format(budget_calculated), file=sys.stderr)
+        if cost_left==0:
+            break
+    if cost_left<oods_left:
+        for i in range(n-1, 0, -1):
+            if cost_left==0:
+                break
+            if budget_left[i]>0:
+                budget_calculated[i] += 1
+                cost_left -=1
+    if cost_left==0:
+        break
+if oods_left==0 and cost_left>0:
+    print("IMPOSSIBLE")
+else:
+    print("budget_calculated {0}".format(budget_calculated), file=sys.stderr)
+    budget_calculated.sort()
+    for b in budget_calculated:
+        print(b)

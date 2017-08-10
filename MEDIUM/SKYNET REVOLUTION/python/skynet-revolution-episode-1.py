0e6783c1972fbe11f9d8225374d34252f6c5adc7
Винямин Карлович Штольц <info@knubiosoft.com>
2017-08-10 10:27:12 +0300

    refactor

---
+++ MEDIUM/SKYNET REVOLUTION/python/skynet-revolution-episode-1.py
@@ -0,0 +1,36 @@
+#https://www.codingame.com/ide/puzzle/skynet-revolution-episode-1
+import sys
+import math
+def getBestBloker(si, gat):
+    global relations
+    for g in gat:
+        if g in relations[si]:
+            return [si, g]
+    for g in gat:
+        if len(relations[g]) > 0:
+            return [g, relations[g][0]]
+    return [0, 0]
+
+
+def unsetter(c1, c2):
+    global relations
+    relations[c1].remove(c2)
+    relations[c2].remove(c1)
+
+
+n, l, e = [int(i) for i in input().split()]
+gat = []
+relations = {}
+for i in range(l):
+    n1, n2 = [int(j) for j in input().split()]
+    relations.setdefault(n1, []).append(n2)
+    relations.setdefault(n2, []).append(n1)
+for i in range(e):
+    ei = int(input())
+    gat.append(ei)
+# game loop
+while True:
+    si = int(input())
+    c1, c2 = getBestBloker(si, gat)
+    unsetter(c1, c2)
+    print("{0} {1}".format(c1, c2))
---
+++ MEDIUM/WAR/python/winamax-battle.py
@@ -0,0 +1,105 @@
+# https://www.codingame.com/ide/puzzle/winamax-battle
+import sys
+import math
+
+
+class Game():
+    KARTS = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13,
+             'A': 14}
+    _player1deck = []
+    _player2deck = []
+    _player1table = []
+    _player2table = []
+    _winner = "PAT"
+    _round = 0
+    _war = False
+
+    def __init__(self):
+        for i in range(int(raw_input())):
+            self._player1deck.append(raw_input())  # the n cards of player 1
+
+        for i in range(int(raw_input())):
+            self._player2deck.append(raw_input())  # the m cards of player 2
+
+    def print_result(self):
+        if self._winner == "PAT":
+            print("PAT")
+        else:
+            print("{0} {1}".format(self._winner, self._round))
+
+    def drag_karts(self, n=1):
+        for i in range(n):
+            self._player1table.append(self._player1deck.pop(0))
+            self._player2table.append(self._player2deck.pop(0))
+
+    def player_got_all(self, np):
+        for kart in self._player1table:
+            if np == 1:
+                self._player1deck.append(kart)
+            else:
+                self._player2deck.append(kart)
+
+        for kart in self._player2table:
+            if np == 1:
+                self._player1deck.append(kart)
+            else:
+                self._player2deck.append(kart)
+
+        self._player1table = []
+        self._player2table = []
+
+    def check_carts(self):
+        p1_kart = self.KARTS[self._player1table[-1][:-1]]
+        p2_kart = self.KARTS[self._player2table[-1][:-1]]
+        if p1_kart > p2_kart:
+            self.player_got_all(1)
+            self._war = False
+        elif p1_kart < p2_kart:
+            self.player_got_all(2)
+            self._war = False
+        else:
+            self._war = True
+
+    def have_enought_karts(self, n):
+        len1, len2 = len(self._player1deck), len(self._player2deck)
+        if len1 >= n and len2 >= n:
+            return True
+        else:
+            # set the winner
+            if self._war:
+                self._winner = "PAT"
+            elif len1 - n < 0 and len2 - n < 0:
+                self._winner = "PAT"
+            elif (len1 > len2):
+                self._winner = "1"
+            else:
+                self._winner = "2"
+
+            return False
+
+    def play(self):
+        if self._war:
+            if self.have_enought_karts(3):
+                self.drag_karts(3)
+            else:
+                return False
+        if self.have_enought_karts(1):
+            if not (self._war):
+                self.round_plus()
+            self.drag_karts()
+            self.check_carts()
+        else:
+            return False
+        return True
+
+    def round_plus(self):
+        self._round = self._round + 1
+
+    def run(self):
+        while self.play():
+            pass
+
+game = Game()
+game.run()
+game.print_result()
+

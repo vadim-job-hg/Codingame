1923941ccb96991e9733abb96ad79fc02c6e1905
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-08-20 22:00:25 +0300

    #

--- CODE GOLF/POWER OF THOR/python3/power-of-thor.py
+++ CODE GOLF/POWER OF THOR/python3/power-of-thor.py
@@ -1,5 +1,2 @@
 x,y,z,w=[int(i)for i in input().split()]
-while 1:
- m,w=(((["",w],["N",w-1])[w>y]),["S",w+1])[w<y]
- m,z=((([m,z],[m+"W",z-1])[z>x]),[m+"E",z+1])[z<x]
- print(m)
+while 1:m,w=(((["",w],["N",w-1])[w>y]),["S",w+1])[w<y]; m,z=((([m,z],[m+"W",z-1])[z>x]),[m+"E",z+1])[z<x];print(m);

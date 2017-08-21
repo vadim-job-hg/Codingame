ac23aa7ea86a5c3de896242140001ca1628a9f75
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-08-21 22:11:02 +0300

    :alien:

--- HARD/THE LABYRINTH/the-labyrinth.py
+++ HARD/THE LABYRINTH/the-labyrinth.py
@@ -1,20 +1,24 @@
 import sys
 import math
-import copy
+
+SCAN_RADIUS = 2
+BACK_DIRECTION = {'LEFT': 'RIGHT', 'RIGHT': 'LEFT', 'UP': 'DOWN', 'DOWN': 'UP'}
+NEXT_DIRECTIONL = {'RIGHT': 'UP', 'UP': 'LEFT', 'LEFT': 'DOWN', 'DOWN': 'RIGHT'}
+NEXT_DIRECTIONR = {'RIGHT': 'DOWN', 'UP': 'RIGHT', 'LEFT': 'UP', 'DOWN': 'LEFT'}
+NEXT_POSITION = {'LEFT': {'rows': 0, 'cols': -1}, 'RIGHT': {'rows': 0, 'cols': 1}, 'UP': {'rows': -1, 'cols': 0},
+                 'DOWN': {'rows': 1, 'cols': 0}}
+
 
 class Action:
     STATUS_SEARCH = 'status_search'
     STATUS_RETURN = 'status_return'
-    BACK_DIRECTION = {'LEFT': 'RIGHT', 'RIGHT': 'LEFT', 'UP': 'DOWN', 'DOWN': 'UP'}
-    NEXT_DIRECTIONL = {'RIGHT': 'UP', 'UP': 'LEFT', 'LEFT': 'DOWN', 'DOWN': 'RIGHT'}
-    NEXT_DIRECTIONR = {'RIGHT': 'DOWN', 'UP': 'RIGHT', 'LEFT': 'UP', 'DOWN': 'LEFT'}
-    NEXT_POSITION = {'LEFT': {'rows': 0, 'cols': -1}, 'RIGHT': {'rows': 0, 'cols': 1}, 'UP': {'rows': -1, 'cols': 0},
-                     'DOWN': {'rows': 1, 'cols': 0}}
     cols = 0
     rows = 0
     countdown = 0
     kirk = {'r': 0, 'c': 0}
-    renember = {}
+    move_history = {}
+    dead_end = {}
+    pos_dir = []
     status = None
     map = []
     direction = 'RIGHT'
@@ -22,6 +26,7 @@ class Action:
     def __init__(self):
         self.status = self.STATUS_SEARCH
         self.rows, self.cols, self.countdown = [int(i) for i in input().split()]
+        self.pos_dir
 
     def scan(self):
         self.kirk['r'], self.kirk['c'] = [int(i) for i in input().split()]
@@ -30,27 +35,52 @@ class Action:
             self.map.append(list(input()))
 
     def get_next(self):
-        return self.map[self.kirk['r'] + self.NEXT_POSITION[self.direction]['rows']][
-            self.kirk['c'] + self.NEXT_POSITION[self.direction]['cols']]
+        return self.map[self.kirk['r'] + NEXT_POSITION[self.direction]['rows']][
+            self.kirk['c'] + NEXT_POSITION[self.direction]['cols']]
+
+    def get_by(self, c, r):
+        return self.map[r][c]
 
     def touch_right_wall(self):
-        self.direction = self.NEXT_DIRECTIONL[self.direction]
+        self.direction = NEXT_DIRECTIONL[self.direction]
         while self.get_next() == '#':
-            self.direction = self.NEXT_DIRECTIONR[self.direction]
+            self.direction = NEXT_DIRECTIONR[self.direction]
 
     def save_direction(self):
-        self.renember[self.kirk['c'] + self.kirk['r'] * self.cols] = self.direction
+        self.move_history[(self.kirk['c'] + NEXT_POSITION[self.direction]['cols'],
+                           self.kirk['r'] + NEXT_POSITION[self.direction]['rows'])] = self.direction
+
+    def load_direction(self, c, r):
+        return self.move_history[(c, r)]
+
+    def ckeck_deadend(self, check):
+        pass
+
+    def can_move(self):
+        pass
 
     def get_direction(self):
-        if (self.map[self.kirk['r']][self.kirk['c']] == 'C'):
-            self.save_direction()
+        if self.get_by(self.kirk['c'], self.kirk['r']) == 'C':
             self.status = self.STATUS_RETURN
-            self.direction = self.BACK_DIRECTION[self.direction]
-        elif self.status is self.STATUS_SEARCH:
-            self.save_direction()
+            # self.direction = BACK_DIRECTION[self.direction]
+        if self.status == self.STATUS_SEARCH:
+            minr = (0, self.kirk['r'] - SCAN_RADIUS)[self.kirk['r'] - SCAN_RADIUS > 0]
+            minc = (0, self.kirk['c'] - SCAN_RADIUS)[self.kirk['c'] - SCAN_RADIUS > 0]
+            maxr = (self.rows, self.kirk['r'] + SCAN_RADIUS)[self.kirk['r'] + SCAN_RADIUS < self.rows]
+            maxc = (self.cols, self.kirk['c'] + SCAN_RADIUS)[self.kirk['c'] + SCAN_RADIUS < self.cols]
+            need_to_check = [
+                {'c': self.kirk['c'], 'r': maxr, 'back': 'UP', 'check': ['LEFT', 'RIGHT', 'DOWN']},
+                {'c': self.kirk['c'], 'r': minr, 'back': 'DOWN', 'check': ['LEFT', 'RIGHT', 'UP']},
+                {'c': maxc, 'r': self.kirk['r'], 'back': 'LEFT', 'check': ['UP', 'RIGHT', 'DOWN']},
+                {'c': minc, 'r': self.kirk['r'], 'back': 'RIGHT', 'check': ['LEFT', 'UP', 'DOWN']},
+            ]
+
+            for check in need_to_check:
+                self.ckeck_deadend(check)
             self.touch_right_wall()
+            self.save_direction()
         else:
-            self.direction = self.BACK_DIRECTION[self.renember[self.kirk['c'] + self.kirk['r'] * self.cols]]
+            self.direction = BACK_DIRECTION[self.move_history[(self.kirk['c'], self.kirk['r'])]]
 
     def run(self):
         print(self.direction)

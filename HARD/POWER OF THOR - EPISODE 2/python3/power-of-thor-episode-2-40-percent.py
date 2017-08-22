0969429d73d2e4feef02700af6cc622b17960b90
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-08-22 09:48:59 +0300

    :alien:

---
+++ HARD/POWER OF THOR - EPISODE 2/python3/power-of-thor-episode-2-40-percent.py
@@ -0,0 +1,72 @@
+import sys
+import math
+
+WIDE = 4
+
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+class Thor:
+    tx = 0
+    ty = 0
+    h = 0
+    n = 0
+    giants = []
+
+    def __init__(self):
+        self.tx, self.ty = [int(i) for i in input().split()]
+
+    def scan(self):
+        self.h, self.n = [int(i) for i in input().split()]
+        self.giants.clear()
+        for i in range(self.n):
+            x, y = [int(j) for j in input().split()]
+            self.giants.append({'x': x, 'y': y})
+
+    def find_the_middle(self):
+        xsumm = ysumm = 0
+        for giant in self.giants:
+            xsumm += giant['x']
+            ysumm += giant['y']
+            print(giant, file=sys.stderr)
+        return int(xsumm / self.n), int(ysumm / self.n)
+
+    def check_if_strike(self):
+        for giant in self.giants:
+            if abs(self.tx - giant['x']) == 1 or abs(self.ty - giant['y']) == 1:
+                return True
+        return False
+
+    def move(self):
+        dir_x, dir_y = self.find_the_middle()
+        print(dir_x, dir_y, self.tx, self.ty, file=sys.stderr)
+        if self.check_if_strike():
+            action = "STRIKE"
+        else:
+            action = ''
+            if self.ty < dir_y:
+                action = action + "S"
+                self.ty += 1
+            elif self.ty > dir_y:
+                action = action + "N"
+                self.ty -= 1
+            if self.tx < dir_x:
+                action = action + "E"
+                self.tx += 1
+            elif self.tx > dir_x:
+                action = action + "W"
+                self.tx -= 1
+        if action == '':
+            action = 'WAIT'
+        print(action)
+
+
+thor = Thor()
+while True:
+    thor.scan()
+    thor.move()
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+    # print(str(current_x), file=sys.stderr)
+

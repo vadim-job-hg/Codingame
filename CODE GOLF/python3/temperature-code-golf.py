c6982c1c7953667fad0b81f92d29ece8bcaa7a52
Vadim Kuznetsov <vadim-job-hg@yandex.ru>
2017-08-23 18:48:26 +0300

    Update temperature-code-golf.py

--- CODE GOLF/python3/temperature-code-golf.py
+++ CODE GOLF/python3/temperature-code-golf.py
@@ -1,5 +1,4 @@
 n,a=int(input()),[int(i)for i in input().split()]
-m=(0,2**9)[n>0]
-for i in a:
- m=(m,i)[abs(m)>abs(i)or(abs(m)==abs(i)and i>m)]
+m=(0,999)[n>0]
+for i in a:m=(m,i)[abs(m)>abs(i)or(abs(m)==abs(i)and i>m)]
 print(m)

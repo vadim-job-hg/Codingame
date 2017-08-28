7a166e4dc37c74e523338c4cbcc1beed62e2dd26
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-08-28 08:38:33 +0300

    :alien:

---
+++ COMMUNITY PUZZLES/THE-GREATEST-NUMBER/python3/the-greatest-number.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+n = int(input())
+maimum = True
+dot = False
+numbers = []
+string = ""
+for i in input().split():
+    if i == '.':
+        dot = True
+    elif i == '-':
+        maimum = False
+    else:
+        numbers.append(int(i))
+
+if maimum:
+    numbers.sort(reverse=True)
+else:
+    numbers.sort()
+    string += "-"
+print(numbers, file=sys.stderr)
+for i in range(len(numbers)):
+    if ((i == len(numbers) - 1 and maimum) or (i == 1 and not (maimum))) and dot:
+        string += "."
+    string += str(numbers[i])
+print(string)

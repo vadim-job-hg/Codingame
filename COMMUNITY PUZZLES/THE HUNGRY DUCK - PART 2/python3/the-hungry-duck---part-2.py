b83c035e833f4a4b6bc18f6943069a173ee9513d
vadim-job-hg <vadim-job-hg@yandex.ru>
2017-09-13 20:35:23 +0300

    :alien:

--- COMMUNITY PUZZLES/THE HUNGRY DUCK - PART 2/python3/the-hungry-duck---part-2.py
+++ COMMUNITY PUZZLES/THE HUNGRY DUCK - PART 2/python3/the-hungry-duck---part-2.py
@@ -6,7 +6,10 @@ def get(x=0, y=0):
     global food
     global w
     global h
-
+    global visited
+    have = visited.get((x,y), False)
+    if have:
+        return have
     current = food[y][x]
     if x + 1 >= w:
         right = 0
@@ -17,9 +20,10 @@ def get(x=0, y=0):
     else:
         down = get(x, y + 1)
     current += (right, down)[down > right]
+    visited[(x,y)] = current
     return current
 
-
+visited = {}
 food = []
 w, h = [int(i) for i in input().split()]
 for i in range(h):
@@ -29,5 +33,5 @@ for i in range(h):
 print(food, file=sys.stderr)
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
-
+#print(1)
 print(get())

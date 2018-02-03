6a58f7572b635801f294b47debb4f22a17fa476b
vadim-job-hg <vadim-job-hg@yandex.ru>
2018-02-03 16:05:56 +0200

    :alien:

--- MULTIPLAYER/CODE4LIFE/python3/code4life-wood-2.py
+++ MULTIPLAYER/CODE4LIFE/python3/code4life-wood-2.py
@@ -6,15 +6,18 @@ DEBUG = True
 
 def debug(*args):
     if (DEBUG):
-        print(args, file=sys.stderr)  # Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
+        print(args, file
+              =sys.stderr)  # Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
+
 
 class Project:
     def __init__(self, data):
         self.a, self.b, self.c, self.d, self.e = [int(j) for j in data.split()]
 
+
 class Robot:
     def put_data(self, data):
-        self.target, eta, score, storage_a, storage_b, storage_c, storage_d, storage_e, expertise_a, expertise_b, expertise_c, expertise_d, expertise_e = input().split()
+        self.target, eta, score, storage_a, storage_b, storage_c, storage_d, storage_e, expertise_a, expertise_b, expertise_c, expertise_d, expertise_e = data.split()
         self.eta = int(eta)
         self.score = int(score)
         self.storage_a = int(storage_a)
@@ -27,10 +30,24 @@ class Robot:
         self.expertise_c = int(expertise_c)
         self.expertise_d = int(expertise_d)
         self.expertise_e = int(expertise_e)
+        debug(self.target)
+
+    def get_molecules(self):
+        return {'A': self.storage_a, 'B': self.storage_b, 'C': self.storage_c, 'D': self.storage_d, 'E': self.storage_e}
+
+    def get_molecules_count(self):
+        return self.storage_a + self.storage_b + self.storage_c + self.storage_d + self.storage_e
+
+
+class Player(Robot):
+    pass
+
 
 class Molecules:
     def put_data(self, data):
-        self.available_a, self.available_b, self.available_c, self.available_d, self.available_e = [int(j) for j in data.split()]
+        self.available_a, self.available_b, self.available_c, self.available_d, self.available_e = [int(j) for j in
+                                                                                                    data.split()]
+
 
 class Sample:
     def __init__(self, data):
@@ -44,6 +61,10 @@ class Sample:
         self.cost_c = int(cost_c)
         self.cost_d = int(cost_d)
         self.cost_e = int(cost_e)
+
+    def get_cost(self):
+        return {'A': self.cost_a, 'B': self.cost_b, 'C': self.cost_c, 'D': self.cost_d, 'E': self.cost_e}
+
 
 class Game:
     projects = []
@@ -59,11 +80,17 @@ class Game:
         # debug(self.project_count)
         for i in range(self.project_count):
             self.projects.append(Project(input()))
-        for i in range(self.players_count):
+
+        self.players.append(Player())
+        for i in range(1, self.players_count):
             self.players.append(Robot())
-            # debug('projects list', self.projects)
+        debug('projects list', self.players)
 
     def run(self):
+        self._collect_data()
+        self._action()
+
+    def _collect_data(self):
         for i in range(self.players_count):
             self.players[i].put_data(input())
         self.molecules.put_data(input())
@@ -72,8 +99,58 @@ class Game:
         for i in range(self.sample_count):
             self.samples.append(Sample(input()))
 
+    def _get_player_samples(self, player_id):
+        return [x for x in self.samples if x.carried_by == player_id]
+
+    def _get_player_molecules(self, player_id):
+        return [x for x in self.samples if x.carried_by == player_id]
+
+    def _action(self):
+        # debug('samples', self.samples)
+        # debug(self.my_player_id)
+        current_player = self.players[self.my_player_id]
+        player_samples = self._get_player_samples(self.my_player_id)
+        if current_player.target == 'START_POS':
+            print("GOTO DIAGNOSIS")
+        elif current_player.target == 'DIAGNOSIS':
+            avaliable_samples = self._get_player_samples(-1)
+            if len(player_samples) < 3 and len(avaliable_samples) > 0:
+                print("CONNECT {}".format(avaliable_samples[0].sample_id))  # todo: minimum molecules needed
+            else:
+                print("GOTO MOLECULES")
+        elif current_player.target == 'MOLECULES':
+            if len(player_samples) > 0:
+                molecules_nedded = player_samples[0].get_cost()
+                molecules_have = current_player.get_molecules()
+                for key, molecule in molecules_nedded.items():
+                    if molecule > molecules_have[key]:
+                        print("CONNECT {}".format(key))
+                        break
+                else:
+                    print("GOTO LABORATORY")
+            else:
+                print("GOTO DIAGNOSIS")
+        elif current_player.target == 'LABORATORY':
+            if len(player_samples) > 0:
+                molecules_count = current_player.get_molecules_count()
+                if molecules_count > 0:
+                    print("CONNECT {}".format(player_samples[0].sample_id))
+                else:
+                    print("GOTO MOLECULES")
+            else:
+                print("GOTO DIAGNOSIS")
+
+        else:
+            print("GOTO DIAGNOSIS")
+
+        # player_samples = self._get_player_samples(self.my_player_id)
+        # debug('player_samples', player_samples)
+        # if(len(player_samples)==0 or ):
+        # print("GOTO DIAGNOSIS")
+        # elif
+
+
 game = Game()
 # game loop
 while True:
     game.run()
-    print("GOTO DIAGNOSIS")

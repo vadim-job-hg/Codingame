4db910d5f236eefa8e7fa4a84c64010eaed41d02
Vadim Kuznetsov <vadimjobhg86@gmail.com>
2018-02-13 17:13:04 +0200

    :alien:

--- BOT PROGRAMMING/FANTASTIC BITS/python3/___fantastic-bits.py
+++ BOT PROGRAMMING/FANTASTIC BITS/python3/___fantastic-bits.py
@@ -1,8 +1,13 @@
-# https://github.com/waniz/katas_hackerrank_codingame_codewars/blob/master/codingame/contests/fantastic_bits/fantastic_bits.py
-# https://www.codingame.com/ide/puzzle/fantastic-bits
 import sys
 import math
 
+DEBUG = True
+
+
+def debug(*args):
+    if (DEBUG):
+        print(args,
+              file=sys.stderr)  # Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
 
 MAP = [16000, 7500]
 WIZARD_RADIUS = 400
@@ -14,7 +19,6 @@ if MY_TEAM_ID == 0:
 else:
     GOALS = [0, 3750]
 
-
 class Entities:
     entities_id = None
     entity_type = None  # "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
@@ -22,7 +26,7 @@ class Entities:
     y = None
     vx = None
     vy = None
-    state = None        # 1 - wizard grabbed snaffle, 0 - otherwise
+    state = None  # 1 - wizard grabbed snaffle, 0 - otherwise
 
     def __init__(self, data):
         self.entities_id = data[0]
@@ -32,7 +36,6 @@ class Entities:
         self.vx = data[4]
         self.vy = data[5]
         self.state = data[6]
-
 
 class Player:
     x = None
@@ -76,6 +79,8 @@ class Player:
 # game loop
 while True:
     game_entities = []
+    myScore, myMagic = input().split()
+    opponentScore, opponentMagic = input().split()
     entities = int(input())  # number of entities still in game
     for _ in range(entities):
         entity_id, entity_type, x, y, vx, vy, state = input().split()
---
+++ COMMUNITY PUZZLES/ADVANCED TREE/python3/___advanced-tree.py
@@ -0,0 +1,9 @@
+# https://github.com/cpj1352/codingame/blob/master/Community%20game/Advanced%20Tree.py
+# https://www.codingame.com/ide/puzzle/advanced-tree
+s="."
+
+text=["./Directory1/File1","./Directory1/File2","./Directory2/File3","./Directory1/File4","./Directory2/Directory3/File5","./File6"]
+
+
+for i in text:
+    print i.split('/')
